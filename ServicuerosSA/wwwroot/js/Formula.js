4603e2a415c4d8d3842ab32a31ed56a9340e78bf
Gabriel <gap5991@hotmail.com>
2018-06-21 21:14:29 -0500

    campos

--- ServicuerosSA/wwwroot/js/Formula.js
+++ ServicuerosSA/wwwroot/js/Formula.js
@@ -20,7 +20,26 @@ class ClaseFormula {
                 $('#version').val = respuesta[0].version;
                 $('#fechaCreacion').val = respuesta[0].fechaCreacion;
                 $('#nombre').val = respuesta[0].nombre;
-                $('#')
+                $('#tipoProceso').val = respuesta[0].tipoProces;
+                $('#tipoPiel').val = respuesta[0].tipoPiel;
+                $('#fechaVigencia').val = respuesta[0].fechaVigencia;
+                $('#vigencia').val = respuesta[0].vigencia;
+                $('pagina').val = respuesta[0].pagina;
+                $('#fecha').val = respuesta[0].fecha;
+                $('#parada').val = respuesta[0].parada;
+                $('#bombo').val = respuesta[0].bombo;
+                $('#peso').val = respuesta[0].peso;
+                $('cantidad').val = respuesta[0].cantidad;
+                $('#promedio').val = respuesta[0].promedio;
+                $('#autorizado').val = respuesta[0].autorizado;
+                $('#procesado').val = respuesta[0].procesado;
+                $('#entregado').val = respuesta[0].entregado;
+                $('#ingrediente').val = respuesta[0].ingrediente;
+                $('#porcentaje').val = respuesta[0].porcentaje;
+                $('#cantidadFormula').val = respuesta[0].cantidadFormula;
+                $('#tiempo').val = respuesta[0].tiempo;
+                $('#observaciones').val = respuesta[0].observaciones;
+                $('#accion').val = respuesta[0].accion;
 
 
 
