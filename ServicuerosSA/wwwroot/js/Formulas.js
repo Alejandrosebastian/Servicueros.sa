39b83d19b5e7470685c38ce12e0baf07a9844c8c
lleroc Llerena <luis_llerena.88_1@hotmail.com>
2018-06-21 22:18:39 -0500

    cambios

--- ServicuerosSA/wwwroot/js/Formulas.js
+++ ServicuerosSA/wwwroot/js/Formulas.js
@@ -25,4 +25,37 @@ class Formulas {
             }
         });
     }
+    CabeceraFormula(id) {
+        var accion = this.accion;
+        $.post(accion,
+            { id },
+            (respuesta) => {
+                $('#codigo').val = respuesta[0].codigo;
+                $('#version').val = respuesta[0].version;
+                $('#fechaCreacion').val = respuesta[0].fechaCreacion;
+                $('#nombre').val = respuesta[0].nombre;
+                $('#tipoProceso').val = respuesta[0].tipoProces;
+                $('#tipoPiel').val = respuesta[0].tipoPiel;
+                $('#fechaVigencia').val = respuesta[0].fechaVigencia;
+                $('#vigencia').val = respuesta[0].vigencia;
+                $('pagina').val = respuesta[0].pagina;
+                $('#fecha').val = respuesta[0].fecha;
+                $('#parada').val = respuesta[0].parada;
+                $('#bombo').val = respuesta[0].bombo;
+                $('#peso').val = respuesta[0].peso;
+                $('cantidad').val = respuesta[0].cantidad;
+                $('#promedio').val = respuesta[0].promedio;
+                $('#autorizado').val = respuesta[0].autorizado;
+                $('#procesado').val = respuesta[0].procesado;
+                $('#entregado').val = respuesta[0].entregado;
+
+
+
+
+            }
+        );
+    }
+    CuerpoFormula(id) {
+
+    }
 }
