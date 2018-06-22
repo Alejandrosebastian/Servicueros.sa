bef16533d4f619ee7352ada5592209e7a82840bf
lleroc Llerena <luis_llerena.88_1@hotmail.com>
2018-06-21 21:51:08 -0500

    ya

--- ServicuerosSA/Models/PelambreModel.cs
+++ ServicuerosSA/Models/PelambreModel.cs
@@ -264,8 +264,32 @@ namespace ServicuerosSA.Models
         public List<ModeloEncabezadoFormula> ModeloImprimirEncabezadoFormula(int id)
         {
             List<ModeloEncabezadoFormula> lista = new List<ModeloEncabezadoFormula>();
+            //var res = from p in _contexto.Pelambre
+            //          join f in _contexto.Formula on p.FormulaId equals f.FormulaId
+            //          join c in _contexto.Componente on f.FormulaId equals c.FormulaId
+            //          select new
+            //          {
+
+            //          };
+
             var res = from p in _contexto.Pelambre
-                      join 
+                      join pe in _contexto.Personal on p.PersonalId equals pe.PersonalId
+                      join b in _contexto.Bombo on p.BomboId equals b.BomboId
+                      join fo in _contexto.Formula on p.FormulaId equals fo.FormulaId
+                      select new ModeloEncabezadoFormula
+                      {
+                          Bombo = b.Num_bombo.ToString(),
+                          Cantidad = p.TotalPieles.ToString(),
+                          FechaCreacionFormula = fo.Fecha_Creacion.ToString(),
+                          FechaImpresion = DateTime.Now.ToString(),
+                          NombreAutoirzado = pe.Nombres + " " + pe.Apellidos,
+                          FechaValida = DateTime.Now.ToString(),
+                          NombreEntregado = pe.Nombres + " " + pe.Apellidos,
+
+
+
+                      };
+
         }
     }
 }
