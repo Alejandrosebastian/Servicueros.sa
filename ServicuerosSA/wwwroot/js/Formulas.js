16344fbccea459339d93984bfc74224e952d8b44
Gabriel <gap5991@hotmail.com>
2018-06-21 23:08:04 -0500

    CAMBIOS

--- ServicuerosSA/wwwroot/js/Formulas.js
+++ ServicuerosSA/wwwroot/js/Formulas.js
@@ -33,22 +33,18 @@ class Formulas {
                 console.log(respuesta);
                 $('#codigo').val = respuesta[0].codigo;
                 $('#version').val = respuesta[0].version;
-                $('#fechaCreacion').val = respuesta[0].fechaCreacion;
-                $('#nombre').val = respuesta[0].nombre;
-                $('#tipoProceso').val = respuesta[0].tipoProces;
+                $('#fechaCreacion').val = respuesta[0].fechaCreacionFormula;
                 $('#tipoPiel').val = respuesta[0].tipoPiel;
-                $('#fechaVigencia').val = respuesta[0].fechaVigencia;
-                $('#vigencia').val = respuesta[0].vigencia;
-                $('pagina').val = respuesta[0].pagina;
-                $('#fecha').val = respuesta[0].fecha;
+                $('#fechaValida').val = respuesta[0].fechaValida;
+                $('#fechaImpresion').val = respuesta[0].fechaImpresion;
                 $('#parada').val = respuesta[0].parada;
                 $('#bombo').val = respuesta[0].bombo;
                 $('#peso').val = respuesta[0].peso;
                 $('cantidad').val = respuesta[0].cantidad;
                 $('#promedio').val = respuesta[0].promedio;
-                $('#autorizado').val = respuesta[0].autorizado;
-                $('#procesado').val = respuesta[0].procesado;
-                $('#entregado').val = respuesta[0].entregado;
+                $('#autorizado').val = respuesta[0].nombreAutorizado;
+                $('#procesado').val = respuesta[0].nombreProcesado;
+                $('#entregado').val = respuesta[0].nombreEntregado;
             }
         );
     }
