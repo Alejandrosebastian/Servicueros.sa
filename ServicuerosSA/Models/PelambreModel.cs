57b52dbef07c9f8ef2177110214cb15319dc1e90
Sebastian Alejandro <kanfyslol@gmail.com>
2018-06-28 10:26:35 -0500

    ok

--- ServicuerosSA/Models/PelambreModel.cs
+++ ServicuerosSA/Models/PelambreModel.cs
@@ -355,6 +355,7 @@ namespace ServicuerosSA.Models
                 {
                     dato += "<tr><td>ESCURRIR BIEN</td><td></td><td></td><td></td><td></td></tr>";
                 };
+               
                 if (item.Detalle.ToUpper() == "SAL LIMPIA")
                 {
                    dato += "<tr><td>CONTROL</td><td></td><td></td><td></td><td></td></tr>";
