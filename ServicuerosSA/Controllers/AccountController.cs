4eb7ae949fd1728c8bcde2858ed2af3057f85c5b
Gabriel <gap5991@hotmail.com>
2018-07-05 12:48:53 -0500

    cambio

--- ServicuerosSA/Controllers/AccountController.cs
+++ ServicuerosSA/Controllers/AccountController.cs
@@ -233,40 +233,34 @@ namespace ServicuerosSA.Controllers
             {
                 var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
                 var result = await _userManager.CreateAsync(user, model.Password);
-                
-                
-
-             
 
                 if (result.Succeeded)
                 {
                     await _userManager.AddToRoleAsync(user, model.Rol);
 
-                        _logger.LogInformation("User created a new account with password.");
+                    _logger.LogInformation("User created a new account with password.");
 
 
-                        var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                        var callbackUrl = Url.EmailConfirmationLink(user.Id, code, Request.Scheme);
-                        await _emailSender.SendEmailConfirmationAsync(model.Email, callbackUrl);
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    var callbackUrl = Url.EmailConfirmationLink(user.Id, code, Request.Scheme);
+                    await _emailSender.SendEmailConfirmationAsync(model.Email, callbackUrl);
 
 
-                        await _signInManager.SignInAsync(user, isPersistent: false);
-                        _logger.LogInformation("User created a new account with password.");
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    _logger.LogInformation("User created a new account with password.");
 
-                        return RedirectToLocal(returnUrl);
+                    return RedirectToLocal(returnUrl);
                 }
-                    AddErrors(result);
-            
+                AddErrors(result);
+            }
 
             // If we got this far, something failed, redisplay form
             return View(model);
         }
 
 
-                // If we got this far, something failed, redisplay form
-                return View(model);
             
-        }
+        
 
        
         
--- ServicuerosSA/Models/ClasificacionTripa.cs
+++ ServicuerosSA/Models/ClasificacionTripa.cs
@@ -12,9 +12,6 @@ namespace ServicuerosSA.Models
         [Display(Name = "Tipo de Clasificacion")]
         [Required(ErrorMessage = "Campo Obligatorio")]
         public string Detalle { get; set; }
-        [Display(Name = "Peso de la clasificacion en kilos")]
-        [Required(ErrorMessage = "Campo Obligatorio")]
-        public int peso { get; set; }
-
+       
     }
 }
