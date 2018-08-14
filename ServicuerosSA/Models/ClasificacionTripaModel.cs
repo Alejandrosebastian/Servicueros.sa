261893d0d8cc0380a657f8d3926707921b2a3798
Gabriel <gap5991@hotmail.com>
2018-08-13 22:56:09 -0500

    modal

---
+++ ServicuerosSA/Models/ClasificacionTripaModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ServicuerosSA.Data;
+namespace ServicuerosSA.Models
+{
+    public class ClasificacionTripaModel
+    {
+        ApplicationDbContext _contexto;
+        public ClasificacionTripaModel(ApplicationDbContext contexto)
+        {
+            _contexto = contexto;
+        }
+        public List<ClasificacionTripa> ClaseModeloListaClasificacionTripa()
+        {
+            return _contexto.ClasificacionTripa.OrderBy(c => c.Detalle).ToList();
+        }
+        //public List<object[]>ModeloFiltrarClasificacionTripa(int id)
+        //{
+        //    List<object[]> lista = new List<object[]>();
+        //    string res = "";
+        //    var datos = ();
+        //}
+    }
+}
