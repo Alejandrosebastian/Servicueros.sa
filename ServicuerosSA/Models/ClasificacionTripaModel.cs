2cf8b71a70bc4d86af120a2e7db4717afc260e26
Sebastian Alejandro <kanfyslol@gmail.com>
2018-08-14 00:03:35 -0500

    ok

--- ServicuerosSA/Models/ClasificacionTripaModel.cs
+++ ServicuerosSA/Models/ClasificacionTripaModel.cs
@@ -16,11 +16,33 @@ namespace ServicuerosSA.Models
         {
             return _contexto.ClasificacionTripa.OrderBy(c => c.Detalle).ToList();
         }
-        //public List<object[]>ModeloFiltrarClasificacionTripa(int id)
-        //{
-        //    List<object[]> lista = new List<object[]>();
-        //    string res = "";
-        //    var datos = ();
-        //}
+       public List<object[]>ModeloFiltrarClasificacionTripa(int id)
+       {
+           List<object[]> lista = new List<object[]>();
+           string datos = "";
+            var res = (from de in _contexto.Descarne
+                       join pe in _contexto.Pelambre on de.PelambreId equals pe.PelambreId
+                       where pe.PelambreId == 9
+                       select new
+                       {
+                           pe.CodigoLote,
+                           pe.Codigo,
+                           de.Cantidad,
+                           pe.Peso
+
+                       }).ToList();
+            foreach(var item in res)
+            {
+                datos ="<tr>"+
+                    "<td>" + item.CodigoLote + ""+ item.Codigo + "</td>" +
+                    "<td>" + item.Cantidad + "</td>" +
+                    "<td>" + item.Peso + "</td>" +
+                   
+                    "</tr>";
+            }
+            object[] objetodatos = { datos };
+            lista.Add(objetodatos);
+            return lista;
+       }
     }
 }
