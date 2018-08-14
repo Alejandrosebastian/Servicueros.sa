b7bb1da6ab0be6e3f70569b0d79055b0cb9ae94d
Gabriel <gap5991@hotmail.com>
2018-08-14 00:33:43 -0500

    js

---
+++ ServicuerosSA/wwwroot/js/ClasificacionTripa.js
@@ -0,0 +1,21 @@
+﻿class ClasificacionTripa {
+    constructor(detalle, accion) {
+        this.detalle = detalle;
+        this.accion = accion;
+    }
+    ClaseListaClasificacionTripa() {
+        var accion = this.accion;
+        var contador = 1;
+        $.ajax({
+            type: "POST",
+            url: accion,
+            data: {},
+            success: (respuesta) => {
+                for (var i = 0; i < respuesta.length; i++) {
+                    document.getElementById('clasificaciontripa').options[contador] = new Option(respuesta[i].detalle, respuesta[i].ClasificacionTripaId);
+                    contador++;
+                }
+            }
+        });
+    }
+}
