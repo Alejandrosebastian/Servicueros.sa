ea18bcf044e086ae3f7de14ab4c90ecb58c3b1d1
Gabriel <gap5991@hotmail.com>
2018-08-14 00:55:48 -0500

    bodegatripa

---
+++ ServicuerosSA/wwwroot/js/BodegaTripa.js
@@ -0,0 +1,19 @@
+﻿class BodegaTripa {
+    constructor(numeropieles, peso, accion) {
+        this.numeropieles = numeropieles;
+        this.peso = peso;
+        this.accion = accion;
+    }
+    bodegatripa() {
+        var bodegatripaId = this.numeropieles;
+        var accion = this.accion;
+        $.ajax({
+            type: "POST",
+            url: accion,
+            data: { bodegatripaId },
+            success: (respuesta) => {
+                $('#BodegaTripa').html(respuesta[0]);
+            }
+        });
+    }
+}
