a9126de8b139a7001af5e2345d02a49c897095ef
Gabriel <gap5991@hotmail.com>
2018-08-23 16:06:02 -0500

    YA

--- ServicuerosSA/Migrations/20180823210250_iniii.Designer.cs
+++ ServicuerosSA/Migrations/20180823210250_iniii.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace ServicuerosSA.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180823210250_iniii")]
+    partial class iniii
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ServicuerosSA/Migrations/20180823210250_iniii.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ServicuerosSA.Migrations
+{
+    public partial class iniii : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "codigodescarne",
+                table: "Descarne",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "codigodescarne",
+                table: "Descarne");
+        }
+    }
+}
