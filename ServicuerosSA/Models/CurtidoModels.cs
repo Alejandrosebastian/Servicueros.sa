b92f97fd7ba86956d68bb621270d8a270003e6ad
Sebastian Alejandro <kanfyslol@gmail.com>
2018-11-07 15:48:07 -0500

    consulta

--- ServicuerosSA/Models/CurtidoModels.cs
+++ ServicuerosSA/Models/CurtidoModels.cs
@@ -79,4 +79,22 @@ namespace ServicuerosSA.Models
         }
 
     }
+//    from cu in Curtidos
+// join bo in Bombos on cu.BomboId equals bo.BomboId
+// join fo in Formulas on cu.FormulaId equals fo.FormulaId
+// join bod in Bodegatripas on cu.BodegaTripaId equals bod.BodegaTripaId
+// join clasi in ClasificacionTripas on bod.ClasificacionTripaId equals clasi.ClasificacionTripaId
+// join me in Medidas on cu.MedidaId equals me.MedidaId
+// join bode in Bodegas on cu.BodegaId equals bode.BodegaId
+
+
+// select new {
+//bo.Num_bombo,
+//fo.Nombre,
+//clasi.Detalle,
+//me.Abreviatura,
+//bode.NombreBodega
+
+
+}
 }
